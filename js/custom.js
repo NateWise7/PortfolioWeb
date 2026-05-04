6ca2e00a87e0ca15292e1ffe61b32bb92652acbd
NateWise7 <nateswise@gmail.com>
2026-05-04 15:37:41 -0600

    fixing layout of what I do, redesigning layout for portfolio items and adding info. added pygame project

--- js/custom.js
+++ js/custom.js
@@ -1,15 +1,4 @@
 
-//
-$(document).ready(function() {
-			$('#fullpage').fullpage({
-				'verticalCentered': false,
-				'scrollingSpeed': 600,
-				'autoScrolling': false,
-				'css3': true,
-				'navigation': true,
-				'navigationPosition': 'right',
-			});
-		});
 
 // wow
 $(function()
